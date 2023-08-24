0917a302ba8bfbeba3432cdd27469b7a07eb966c
Xian <rita910812@gmail.com>
2023-08-24 07:53:28 +0000

    add reference use(connect blob)

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
+import io
 import logging
 import openai
+import mimetypes
 from flask_cors import CORS
 from decouple import config
 from flask import Flask, request, jsonify, send_file, abort
 from azure.identity import DefaultAzureCredential
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
+from azure.storage.blob import BlobServiceClient
 from approaches.chatreadretrieveread import ChatReadRetrieveReadApproach
 from retry import retry
 from wrapt_timeout_decorator import timeout
@@ -16,6 +19,7 @@ from openai import error
 AZURE_STORAGE_ACCOUNT = os.environ.get("AZURE_STORAGE_ACCOUNT") or "mystorageaccount"
 AZURE_STORAGE_CONTAINER = os.environ.get("AZURE_STORAGE_CONTAINER") or "content"
 AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE") or "gptkb"
+AZURE_STORAGE_KEY = os.environ.get("AZURE_STORAGE_KEY") or "mykey"
 AZURE_SEARCH_INDEX = os.environ.get("AZURE_SEARCH_INDEX") or "gptkbindex"
 AZURE_SEARCH_SERVICE_KEY = os.environ.get("AZURE_SEARCH_SERVICE_KEY") or "mykey"
 AZURE_OPENAI_SERVICE = os.environ.get("AZURE_OPENAI_SERVICE") or "myopenai"
@@ -40,6 +44,7 @@ KB_FIELDS_SOURCEPAGE = os.environ.get("KB_FIELDS_SOURCEPAGE") or "sourcepage"
 print('AZURE_STORAGE_ACCOUNT= ', AZURE_STORAGE_ACCOUNT)
 print('AZURE_STORAGE_CONTAINER= ', AZURE_STORAGE_CONTAINER)
 print('AZURE_SEARCH_SERVICE= ', AZURE_SEARCH_SERVICE)
+print('AZURE_STORAGE_KEY= ', AZURE_STORAGE_KEY)
 print('AZURE_SEARCH_INDEX= ', AZURE_SEARCH_INDEX)
 print('AZURE_OPENAI_SERVICE= ', AZURE_OPENAI_SERVICE)
 print('AZURE_OPENAI_CHATGPT_DEPLOYMENT= ', AZURE_OPENAI_CHATGPT_DEPLOYMENT)
@@ -51,7 +56,7 @@ print(KB_FIELDS_CONTENT, KB_FIELDS_CATEGORY, KB_FIELDS_SOURCEPAGE)
 # just use 'az login' locally, and managed identity when deployed on Azure). If you need to use keys, use separate AzureKeyCredential instances with the 
 # keys for each service
 # If you encounter a blocking error during a DefaultAzureCredntial resolution, you can exclude the problematic credential by using a parameter (ex. exclude_shared_token_cache_credential=True)
-azure_credential = DefaultAzureCredential()
+azure_credential = AZURE_STORAGE_KEY# DefaultAzureCredential()
 azure_credential_search = AzureKeyCredential(AZURE_SEARCH_SERVICE_KEY)
 # Used by the OpenAI SDK
 openai.api_type = "azure"
@@ -69,6 +74,11 @@ search_client = SearchClient(
     index_name=AZURE_SEARCH_INDEX,
     credential=azure_credential_search)
 
+blob_client = BlobServiceClient(
+    account_url=f"https://{AZURE_STORAGE_ACCOUNT}.blob.core.windows.net", 
+    credential=azure_credential)
+blob_container = blob_client.get_container_client(AZURE_STORAGE_CONTAINER)
+
 chat_approaches = {
     "rrr": ChatReadRetrieveReadApproach(search_client, AZURE_OPENAI_CHATGPT_DEPLOYMENT, KB_FIELDS_SOURCEPAGE, KB_FIELDS_CONTENT)
 }
@@ -80,6 +90,22 @@ CORS(app)
 @app.route("/<path:path>")
 def static_file(path):
     return app.send_static_file(path)
+
+# Serve content files from blob storage from within the app to keep the example self-contained. 
+# *** NOTE *** this assumes that the content files are public, or at least that all users of the app
+# can access all the files. This is also slow and memory hungry.
+@app.route("/content/<path>")
+def content_file(path):
+    blob = blob_container.get_blob_client(path).download_blob()
+    if not blob.properties or not blob.properties.has_key("content_settings"):
+        abort(404)
+    mime_type = blob.properties["content_settings"]["content_type"]
+    if mime_type == "application/octet-stream":
+        mime_type = mimetypes.guess_type(path)[0] or "application/octet-stream"
+    blob_file = io.BytesIO()
+    blob.readinto(blob_file)
+    blob_file.seek(0)
+    return send_file(blob_file, mimetype=mime_type, as_attachment=False, download_name=path)
 
 @retry((TimeoutError, error.Timeout), logger=logging.getLogger(__name__), delay=2)
 @timeout(40)
